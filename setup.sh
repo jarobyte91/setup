35205f6abfbf6512656e4b404250f9e1245f55a2
Juan Ramirez <caballazo.loco@gmail.com>
2017-05-25 14:01:50 -0500

    bookdown working

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ apt-get install libxml2-dev libssl-dev libcurl4-openssl-dev gfortran build-essen
 echo "deb https:cran.itam.mx/bin/linux/ubuntu xenial/">>/etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get install r-base
+apt-get install texlive-latex-recommended texlive-xetex
+apt-get install lmodern
 Rscript setup.R
 # git
 apt-get install git
