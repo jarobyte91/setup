4564808bead96fc793d0167bdf298f40a7ede382
Juan Ramirez <caballazo.loco@gmail.com>
2017-07-12 14:30:41 -0500

    python added

--- setup.sh
+++ setup.sh
@@ -8,4 +8,11 @@ apt-get install lmodern
 Rscript setup.R
 # git
 apt-get install git
+# python
+sudo apt install spyder3 -y
+sudo apt install python3-pip -y
+sudo apt install python3-dev -y
+yes|sudo -H pip3 install setuptools
+yes|sudo -H pip3 install -U spyder
+yes|sudo -H pip3 install tensorflow
 
