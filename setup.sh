d9e74f20bd7e2be4c8a9a9b9e26bcd6d1a49bb07
Juan Ramirez-Orta <jarobyte91@gmail.com>
2021-07-09 15:23:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/ins
 # installing
 sudo apt update
 
-sudo apt install emacs vim inkscape okular zotero xournalpp nvidia-driver-460 nvidia-utils-460
+sudo apt install emacs vim inkscape okular zotero xournalpp #nvidia-driver-460 nvidia-utils-460
 
 cp vimrc ~/.vimrc
 cp emacs ~/.emacs
