9e358f58160cd6d31ee74faa996600afa2785cc8
Juan <jarobyte91@gmail.com>
2021-11-02 00:10:10 -0300

    removed xournalpp

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # adding repos
-sudo add-apt-repository ppa:apandada1/xournalpp-stable
+# sudo add-apt-repository ppa:apandada1/xournalpp-stable
 
 # external files
 wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash
@@ -7,7 +7,7 @@ wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/ins
 # installing
 sudo apt update
 
-sudo apt install emacs vim inkscape okular zotero xournalpp #nvidia-driver-460 nvidia-utils-460
+sudo apt install emacs vim inkscape okular zotero # xournalpp nvidia-driver-460 nvidia-utils-460
 
 cp vimrc ~/.vimrc
 cp emacs ~/.emacs
