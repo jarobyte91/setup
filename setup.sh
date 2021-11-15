cdcc5c173a4b10b7b045fed8b11894f1b42980ff
Juan Ramirez-Orta <jarobyte91@gmail.com>
2021-11-15 13:40:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 # adding repos
 # sudo add-apt-repository ppa:apandada1/xournalpp-stable
+# sudo apt update
+# sudo apt install xournalpp 
 
-# external files
+# zotero
 wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash
-
-# installing
 sudo apt update
+sudo apt install zotero
 
-sudo apt install emacs vim inkscape okular zotero # xournalpp nvidia-driver-460 nvidia-utils-460
+# general
+sudo apt update
+sudo apt install emacs 
+sudo apt install vim 
+sudo apt install inkscape 
+sudo apt install okular 
+sudo apt gimp
 
 cp vimrc ~/.vimrc
 cp emacs ~/.emacs
