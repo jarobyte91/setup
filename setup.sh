f04dd56880e06e08b3a5708527c79322a538cd85
Juan Ramirez-Orta <jarobyte91@gmail.com>
2022-01-18 22:55:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,21 +6,21 @@
 # zotero
 wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash
 sudo apt update
-sudo apt install zotero
+sudo apt install zotero -y
 
 # R
 sudo apt update
-sudo apt install r-base
-sudo apt install r-cran-tidyverse
+sudo apt install r-base -y
+sudo apt install r-cran-tidyverse -y
 # visit https://github.com/IRkernel/IRkernel to install R into Jupyter
 
 # general
 sudo apt update
-sudo apt install emacs 
-sudo apt install vim 
-sudo apt install inkscape 
-sudo apt install okular 
-sudo apt gimp
+sudo apt install emacs -y 
+sudo apt install vim -y 
+sudo apt install inkscape -y
+sudo apt install okular -y
+sudo apt install gimp -y
 
 cp vimrc ~/.vimrc
 cp emacs ~/.emacs
