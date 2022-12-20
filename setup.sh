f92cc59196ab3a14d8e916807a26117f53a6a6db
Juan Ramirez-Orta <jarobyte91@gmail.com>
2022-12-19 23:31:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
+sudo apt update
+
 # xournal
 sudo add-apt-repository ppa:apandada1/xournalpp-stable
-sudo apt update
 sudo apt install xournalpp -y
 
 # zotero
 wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash
-sudo apt update
 sudo apt install zotero -y
 
 # R
-sudo apt update
 sudo apt install r-base -y
 sudo apt install r-cran-tidyverse -y
 # visit https://github.com/IRkernel/IRkernel to install R into Jupyter
 
 # general
-sudo apt update
 sudo apt install emacs -y 
 sudo apt install vim -y 
 sudo apt install inkscape -y
